9b3cd9651300153d3bdd0fda0a6d93d825a9646f
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-07-09 18:02:27 +0000

    Deploying to gh-pages from @ katesyvash795/goit-js-hw-09@95c3c30de14ef7ad34de8ad7780c14fd013ff330 🚀

---
+++ 01-color-switcher.4b61ebb0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.4b61ebb0.js.map
---
+++ 01-color-switcher.d11b50b1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.d11b50b1.js.map
